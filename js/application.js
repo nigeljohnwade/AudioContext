0de4f4d1470a2d8fd85264ca6fc08e228a4c21c8
Nigel Wade <nigeljohnwade@gmail.com>
2016-08-13 12:09:13 +0100

    Add interface for filter

--- js/application.js
+++ js/application.js
@@ -1,9 +1,11 @@
 requirejs(['audioContext'], function(audioContext){
     window.context = audioContext.init();
     console.log(context);
-    window.filter1 = audioContext.createBiquadFilterNode('bandpass', 2000, 1, null, context.destination);
-    window.osc1 = audioContext.createOscillatorNode('sawtooth', 220, context, filter1);
-    osc1.start(0);
-    window.osc2 = audioContext.createOscillatorNode('square', 110, context, filter1);
-    osc2.start(0);
+    window.filter1 = audioContext.createBiquadFilterNode('lowpass', 2000, 1, null, context.destination);
+    window.playOscillators = function(time){
+        window.osc1 = audioContext.createOscillatorNode('sawtooth', 220, context, filter1);
+        window.osc2 = audioContext.createOscillatorNode('square', 110, context, filter1);
+        osc1.start(time);
+        osc2.start(time);
+    }
 });
